a0e026aac7d101a87b77b40c4a59c88c2f34ca47
Sam Serpoosh <saserpoosh@groupon.com>
2016-02-26 17:27:53 -0600

    Remove old and irrelevant personal config and put template

--- src/CategoryConfig.hs
+++ src/CategoryConfig.hs
@@ -3,17 +3,16 @@ module CategoryConfig where
 import Category
 
 -- Predefined categories and their threshold which can change
-food, transportation, monthly, grocery, clothes, books, fun, medical, others :: Category
+food, transportation, monthly, grocery, clothes, books, fun, others :: Category
 
-food           = Cat { name = "food",            threshold = 200  }
-transportation = Cat { name = "transportation",  threshold = 200  }
-monthly        = Cat { name = "monthly",         threshold = 2000 }
-grocery        = Cat { name = "grocery",         threshold = 300  }
-clothes        = Cat { name = "clothes",         threshold = 300  }
-books          = Cat { name = "books",           threshold = 100  }
-fun            = Cat { name = "fun",             threshold = 100  }
-medical        = Cat { name = "medical",         threshold = 400  }
-others         = Cat { name = "others",          threshold = 300  }
+food           = Cat { name = "food",            threshold = 0.00 }
+transportation = Cat { name = "transportation",  threshold = 0.00 }
+monthly        = Cat { name = "monthly",         threshold = 0.00 }
+grocery        = Cat { name = "grocery",         threshold = 0.00 }
+clothes        = Cat { name = "clothes",         threshold = 0.00 }
+books          = Cat { name = "books",           threshold = 0.00 }
+fun            = Cat { name = "fun",             threshold = 0.00 }
+others         = Cat { name = "others",          threshold = 0.00 }
 
 validCategories :: [Category]
 validCategories = [
@@ -24,6 +23,5 @@ validCategories = [
                     clothes,
                     books,
                     fun,
-                    medical,
                     others
                   ]
